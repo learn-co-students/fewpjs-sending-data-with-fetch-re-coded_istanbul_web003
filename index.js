36914fc0137e1f94cb01d0cf67bcbfb984944b5a
bertac_severcan <bertac.severcan@icloud.com>
2020-07-31 20:06:24 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Add your code here
+function submitData(userName, userEmail){
+    let data = {name: userName, email: userEmail};
+    let  configObj = {
+        method: "POST",
+        headers: {"Content-Type": "application/json",
+    "Accept": "application/json"},
+    
+    body : JSON.stringify(data)
+    };
+    return fetch("http://localhost:3000/users", configObj)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(object){
+        console.log(object);
+        document.body.innerHTML = object.id;
+
+    })
+    .catch(function(error){
+        console.log(error.message);
+        document.body.innerHTML += error.message;
+
+    });
+
+}
+//submitData("Bert","bertac@mail");
