4e3a141bb69e0b780e5504e7aa59ccfd09049743
mohammad-jundi <mohammad.jundi1990@gmail.com>
2020-08-09 01:58:42 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Add your code here
+let submitData = {
+    userName: "",
+    userEmail: ""
+  };
+let confirmData = {
+   headers: {
+    "Content-Type": "application/json",
+    "Accept": "application/json"
+   }, 
+   body: JSON.stringify(submitData)
+};
+
+fetch("http://localhost:3000/users", confirmData)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(object) {
+        console.log(object);
+    })
+    .catch(function(err) {
+        alert("ERORE");
+        console.log(err.message);
+    });
