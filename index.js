7ac59b82ba98415da8dd4468600d749d8066621c
Hiba <hiba.mashfij@gmail.com>
2020-08-11 13:27:39 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
-// Add your code here
+
+
+function submitData(userName, userEmail) {
+    
+ 
+    const body = document.querySelector("body");
+    let configObj = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name: userName,
+            email: userEmail
+        })
+    };
+    return fetch("http://localhost:3000/users", configObj)
+        .then(response => {
+            return response.json();
+        })
+        .then(json => {
+            
+             body.innerHTML+= Object.entries(json)
+        
+
+        })
+
+        .catch(function(error){
+            alert("Problem");
+              //  console.log(error.message);
+
+            const problem = document.createElement('p')
+            problem.innerHTML = error.message;
+            body.appendChild(problem)
+
+        });
+
+     
+
+
+}
+
+  submitData('steve', 'hello')
+
+
+
+
+
